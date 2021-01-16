066659a170ee776295a585d2120e9c6bedf955ce
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2021-01-16 21:54:48 +0800

    Adding initial markdown and sample image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React, { useState } from "react";
 import DOMPurify from "dompurify";
 const marked = require("marked");
+import image from "./image.jpg";
+
+marked.setOptions({
+  gfm: true,
+  breaks: true,
+});
+
+const defaultText = `\n
+# Heading 1
+## Heading 2
+[Link](https://www.google.com)  
+\`inline code\`
+\`\`\`
+a
+code
+block
+\`\`\`
+- List item 0
+- List item 1
+- List item 2
+
+> A block quote
+
+**bold text**
+
+![Image](${image})
+`;
 
 export const App = () => {
-  const [textMd, setTextMd] = useState("sdfsdf");
+  const [textMd, setTextMd] = useState(defaultText);
 
   return (
     <>
