34a8abce1be29f1d5354568cd60bc2e1ed43636f
Erwin Cabrera <erwin.d.cabrera@gmail.com>
2021-01-17 16:59:00 +0800

    Initial styles

--- src/App.js
+++ src/App.js
@@ -8,16 +8,20 @@ marked.setOptions({
   breaks: true,
 });
 
-const defaultText = `\n
-# Heading 1
+const defaultText = `# Heading 1
+
 ## Heading 2
+
 [Link](https://www.google.com)  
+
 \`inline code\`
+
 \`\`\`
 a
 code
 block
 \`\`\`
+
 - List item 0
 - List item 1
 - List item 2
@@ -33,16 +37,18 @@ export const App = () => {
   const [textMd, setTextMd] = useState(defaultText);
 
   return (
-    <>
+    <div className="app">
       <textarea
+        className
         id="editor"
         value={textMd}
         onChange={(e) => setTextMd(e.target.value)}
       />
       <p
+        className="preview"
         id="preview"
         dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(marked(textMd)) }}
       ></p>
-    </>
+    </div>
   );
 };
